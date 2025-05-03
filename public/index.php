c71bf60e7c012e729e57c15988b5b052eab3779a
sam0rr <Samor140@hotmail.com>
2025-05-02 20:59:52 -0400

    feat : handle authentication errors

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
+// IGNORE DEPRECATED ERRORS
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 define('ROOT_DIR', realpath(__DIR__ . '/..'));
+
 require ROOT_DIR . '/vendor/autoload.php';
 
 $kernel = new Models\Core\Kernel();
