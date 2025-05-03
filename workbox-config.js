3db636148f29d9281c40014f89d797cd40c94de0
sam0rr <Samor140@hotmail.com>
2025-05-03 05:01:13 -0400

    feat : final workbox

--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,3 @@
-require('dotenv').config();
-
-const FONT_TTL = parseInt(
-    process.env.CACHE_MAX_AGE_SECONDS,
-    10
-) || 60 * 60 * 24 * 365 * 2;
-
 module.exports = {
     globDirectory: 'public',
     globPatterns: [
@@ -13,22 +6,11 @@ module.exports = {
     swDest: 'public/serviceWorker.js',
     runtimeCaching: [
         {
-            urlPattern: /\.(?:woff2?|ttf|eot)$/i,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'font-cache-v1',
-                expiration: {
-                    maxEntries: 20,
-                    maxAgeSeconds: FONT_TTL,
-                },
-            },
-        },
-        {
             urlPattern: /\/.*/i,
             handler: 'NetworkFirst',
             options: {
                 cacheName: 'runtime-cache',
             },
-        },
-    ],
+        }
+    ]
 };
